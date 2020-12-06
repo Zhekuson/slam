c767edf3f8582ae1bcd3a5ebe428550782a88257
Zhekuson <zhekuson@gmail.com>
2020-12-06 18:06:49 +0300

    modified scene

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -24,8 +24,8 @@ class ImageProcessor():
         self.detection_model = ObjectDetectionModel.load_model()
         self.limit = 0.95
         self.last_update_time = 0
-        self.transform = A.Compose([TT.ToTensor()])
-        self.transform1 = A.Compose([TT.ToTensor()])
+        self.transform = A.Compose([A.Resize(256,256),TT.ToTensor()])
+        self.transform_depth = A.Compose([A.Resize(512,512),TT.ToTensor()])
         self.counter = 0
         self.lock = Lock()
 
@@ -36,39 +36,40 @@ class ImageProcessor():
             if not msg[0]:
                 raise RobotException("ERROR IN GETTING IMAGE FROM SCANNER")
             input = np.asarray(bytearray(msg[2]), dtype=np.uint8)
+            #image = input.reshape((3, msg[1][0], msg[1][1]))
             image = input.reshape((msg[1][0], msg[1][1], 3))
-            
+            #tensor = torch.tensor(image).float()
+            #tensor2 = torch.tensor(image).float()
             tensor = self.transform(image = image, bboxes = None, labels=None)["image"]
-            tensor2 = self.transform1(image = image, bboxes = None, labels = None)["image"]
+            tensor2 = self.transform_depth(image = image, bboxes = None, labels = None)["image"]
             
-
+            start_time = time.time()
             depth_tensor = self.depth_model(torch.reshape(tensor2,(1,tensor2.size()[0],
                 tensor2.size()[1],tensor2.size()[2])).float())
-            start_time = time.time()
-            depth_tensor = (depth_tensor - torch.min(depth_tensor)) / (torch.max(depth_tensor) - torch.min(depth_tensor))
             end_time = time.time()
             print("depth estimation took: "+str(end_time-start_time)+' sec')
+            depth_tensor = (depth_tensor - torch.min(depth_tensor)) / (torch.max(depth_tensor) - torch.min(depth_tensor))
             depth_image = torchvision.transforms.ToPILImage()(depth_tensor.squeeze(0))
             print(depth_image.size)
             
-            #plt.imshow(depth_image,cmap='gray')
-            #plt.show()
+            plt.imshow(depth_image,cmap='gray')
+            plt.show()
             start_time = time.time()
             detection_predict = self.detection_model([tensor])
             end_time = time.time()
             print("detection took: "+str(end_time-start_time)+' sec')
             if(detection_predict[0]['boxes'].size()[0]!=0):
                 
-                image = torchvision.transforms.ToPILImage()(tensor)
-                print(image.size)
+                image = torchvision.transforms.ToPILImage()(tensor).resize((128,128))
+                print('image size',image.size)
                 draw = ImageDraw.Draw(image)
 
                 for index, box in enumerate(detection_predict[0]['boxes']):
                     if(detection_predict[0]['scores'][index] > self.limit):
                         draw.rectangle([(box[0], box[1]), (box[2], box[3])], outline='white')
                   
-                #plt.imshow(image)
-                #plt.show()
+                plt.imshow(image)
+                plt.show()
 
             self.map_builder_callback(image)
             self.last_update_time = time.time() 
